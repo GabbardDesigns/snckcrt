ab773e75b1d86a338ca2b2519e8c8c53bf7f847c
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-07-16 18:08:57 -0400

    Modified Admin.py to remove superuser permissions control for non-superuser

--- products/admin.py
+++ products/admin.py
@@ -6,4 +6,31 @@ from .models import Product
 
 admin.site.register(Product)
 
+# modify Admin page
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from copy import deepcopy
+
+
+class UserAdmin(UserAdmin):
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(UserAdmin, self).get_fieldsets(request, obj)
+        if not obj:
+            return fieldsets
+
+        if not request.user.is_superuser or request.user.pk == obj.pk:
+            fieldsets = deepcopy(fieldsets)
+            for fieldset in fieldsets:
+                if 'is_superuser' in fieldset[1]['fields']:
+                    if type(fieldset[1]['fields']) == tuple:
+                        fieldset[1]['fields'] = list(fieldset[1]['fields'])
+                    fieldset[1]['fields'].remove('is_superuser')
+                    break
+
+        return fieldsets
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
