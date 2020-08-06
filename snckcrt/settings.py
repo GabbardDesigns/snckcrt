0b25af4e0738072563f21ee5135c89e8ff21c862
Patrick Gabbard <gabbbarddesigns@gmail.com>
2020-08-06 16:30:59 -0400

    Added AWS settings

--- snckcrt/settings.py
+++ snckcrt/settings.py
@@ -43,11 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
+
 
     # third-party apps
     # 'adminplus',
     'sorl.thumbnail',
+    'storages',
 
     # my apps
     'products',
